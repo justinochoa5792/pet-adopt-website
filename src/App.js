da04a20f01c9b925cd90fa138d4d515cb8c108d3
Justin Ochoa <just.och92@gmail.com>
2020-06-16 13:06:07 -0400

    added api

--- src/App.js
+++ src/App.js
@@ -1,33 +1,18 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
-
-const Pet = (props) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, props.name),
-    React.createElement("h2", {}, props.animal),
-    React.createElement("h2", {}, props.breed),
-  ]);
-};
+import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me"),
-    React.createElement(Pet, {
-      name: "Luna",
-      animal: "Dog",
-      breed: "American Bulldog",
-    }),
-    React.createElement(Pet, {
-      name: "Tom",
-      animal: "Cat",
-      breed: "House Cat",
-    }),
-    React.createElement(Pet, {
-      name: "Jerry",
-      animal: "Mouse",
-      breed: "Mouse",
-    }),
-  ]);
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      {/* <Pet name="Luna" animal="Dog" breed="American Bulldog" />
+      <Pet name="Tom" animal="Cat" breed="TomCat" />
+      <Pet name="Jerry" animal="Mouse" breed="Mouse" /> */}
+      <SearchParams />
+    </div>
+  );
 };
 render(React.createElement(App), document.getElementById("root"));
 
---
+++ src/SearchParams.jsx
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from "react";
+import pet, { ANIMALS } from "@frontendmasters/pet";
+import useDropdown from "./useDropdown";
+
+const SearchParams = () => {
+  const [location, setLocation] = useState("Seattle, Wa");
+  const [breeds, setBreeds] = useState([]);
+  const [animal, AnimalDropdown] = useDropdown("animal", "dog", ANIMALS);
+  const [breed, BreedDropdown, setBreed] = useDropdown("breed", " ", breeds);
+
+  useEffect(() => {
+    setBreeds([]), setBreed("");
+
+    pet.breeds(animal).then(({ breeds }) => {
+      const breedStrings = breeds.map(({ name }) => name);
+      setBreeds(breedStrings);
+    });
+  }, [animal, setBreed, setBreeds]);
+
+  return (
+    <div className="search-params">
+      <form>
+        <label htmlFor="location">
+          location
+          <input
+            id="location"
+            value={location}
+            placeholder="Location"
+            onChange={(e) => setLocation(e.target.value)}
+          />
+        </label>
+        <AnimalDropdown />
+        <BreedDropdown />
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default SearchParams;
